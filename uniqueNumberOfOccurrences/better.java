07f9669fd662f2f457f3f1a6f045d3b67b220e35
Haruki Yoshida <59747549+haruki4299@users.noreply.github.com>
2022-11-30 23:59:23 -0600

    Create better.java

---
+++ uniqueNumberOfOccurrences/better.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        Set<Integer> set = new HashSet<>(map.values());
+        return map.size() == set.size();
+    }
+}
+
+// Runtime better than 88.71%
+// Notice that the hashset size and the original array size should not change.
