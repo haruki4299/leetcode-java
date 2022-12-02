c5bd5572829073c09f6f5cf56b4d69649907f389
Haruki Yoshida <59747549+haruki4299@users.noreply.github.com>
2022-12-02 16:05:26 -0600

    Create otherSolutions.java

---
+++ integerToRoman/otherSolutions.java
@@ -0,0 +1,31 @@
+// This is much cleaner with same idea but has a longer run time
+public static String intToRoman(int num) {
+    String M[] = {"", "M", "MM", "MMM"};
+    String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+}
+
+// faster and with better memory usage.
+class Solution {
+    public String intToRoman(int num) {
+        
+        String[] roman = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] nums = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        
+        String res = "";
+        int i = 0;
+        while(num > 0)
+        {
+            while(num >= nums[i])
+            {
+                res += roman[i];
+                num -= nums[i];
+            }
+            i++;
+        }
+        
+        return res;
+    }
+}
